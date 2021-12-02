e9602a696c9f2548678d526994b4ccc05c907fa0
Eugênio Moreira <eugeniojava@outlook.com>
2021-12-01 21:35:53 -0300

    some change to trigger Travis

--- src/main/java/br/com/parkineasy/service/PagamentoService.java
+++ src/main/java/br/com/parkineasy/service/PagamentoService.java
@@ -3,10 +3,9 @@ package br.com.parkineasy.service;
 import java.math.BigDecimal;
 
 public interface PagamentoService {
-
+    s
     BigDecimal calcularValorPagamento(Integer codigoTicket);
 
     Boolean efetuarPagamento(Integer codigoTicket, Integer metodoPagamento);
 
 }
-
--- src/test/java/br.com.parkineasy.service/PagamentoServiceImplTest.java
+++ src/test/java/br.com.parkineasy.service/PagamentoServiceImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 class PagamentoServiceImplTest {
     @Test
     void calcularValorPagamento() {
-
     }
 
     @Test
