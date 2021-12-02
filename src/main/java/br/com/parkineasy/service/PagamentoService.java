ab5ae21b435858f0c26ccafdaa68ff4cd4bb0291
Eugênio Moreira <eugeniojava@outlook.com>
2021-12-01 21:38:16 -0300

    fix: revert broken code

--- src/main/java/br/com/parkineasy/service/PagamentoService.java
+++ src/main/java/br/com/parkineasy/service/PagamentoService.java
@@ -3,9 +3,7 @@ package br.com.parkineasy.service;
 import java.math.BigDecimal;
 
 public interface PagamentoService {
-    s
     BigDecimal calcularValorPagamento(Integer codigoTicket);
 
     Boolean efetuarPagamento(Integer codigoTicket, Integer metodoPagamento);
-
 }
